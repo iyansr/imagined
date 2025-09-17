f98253de6b23dec4da7f3e260d866ed8232371c8
I Putu Saputrayana <ikhyan7@gmail.com>
2025-09-17 17:27:38 +0800

    fix session

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,12 +1,15 @@
+import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-import { authClient } from '@/lib/auth/auth-client';
+import { type AuthSession, getAuth } from '@/lib/auth/auth';
 import { CreatePromptPage } from '@/modules/prompt/create-prompt-page';
 
 export default async function Page() {
-  const session = await authClient.getSession();
+  const session = (await getAuth().api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  })) as unknown as AuthSession;
 
-  if (!session.data?.user) {
+  if (!session?.user) {
     return redirect('/auth');
   }
 
